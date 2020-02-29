f97ba1a8bf44a19a210e01f4df967b826b7fe4af
dggdesign <45150036+dggdesign@users.noreply.github.com>
2020-02-29 19:36:50 +0100

    Update menu-insertar-ubicacion.php

--- menu-insertar-ubicacion.php
+++ menu-insertar-ubicacion.php
@@ -3,12 +3,14 @@
 // Crear Ubicación de Menú
 // Generate WP - https://generatewp.com/nav-menus/
 
+// WP docs - https://developer.wordpress.org/reference/functions/register_nav_menus/
+
 if ( ! function_exists( 'ubicacion_menu' ) ) {
   function ubicacion_menu() {
     $locations = array(
-  'ubicacion' => __( 'Ubicacion', 'ubicacion_menu' ),
-  );
-  register_nav_menus( $locations );
+      'ubicacion' => __( 'Ubicacion', 'ubicacion_menu' ),
+    );
+    register_nav_menus( $locations );
   }
   add_action( 'init', 'ubicacion_menu' );
 }
